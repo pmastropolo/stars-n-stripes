586366146e0abbf2474d9c0e31f7dae33bd92210
pmastropolo <amyhytwn@aol.com>
2025-02-03 15:25:27 -0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://starsnstripesautomotive.com", // Your website's URL
+      outDir: 'dist',  // This specifies where to generate the sitemap.xml
+    }),
+  ],
   build: {
     sourcemap: true,  // optional for better debugging
   },
